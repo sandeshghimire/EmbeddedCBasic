3c4c694c2a9a565b81d6f04a79859d179a2b6269
Sandesh Ghimire <sandeshghimire@outlook.com>
2016-11-23 11:58:00 -0800

    toggle bits

--- src/toggleBits.c
+++ src/toggleBits.c
@@ -1,6 +1,94 @@
+/*
+ * Copyright 2016, Sandesh J. Ghimire
+ * All Rights Reserved.
+ */
+
+/** @file
+ * clearBits.c
+ * This Exmaple shows how to toggle a bit on a unigned int 
+ */
+
+/******************************************************
+ *                      Include Files
+ ******************************************************/
+
 #include <stdio.h>
 
-int main(int argc, char* argv)
+/******************************************************
+ *                      Macros
+ ******************************************************/
+
+/******************************************************
+ *                    Constants
+ ******************************************************/
+
+/******************************************************
+ *                   Enumerations
+ ******************************************************/
+
+/******************************************************
+ *                 Type Definitions
+ ******************************************************/
+
+/******************************************************
+ *                    Structures
+ ******************************************************/
+
+/******************************************************
+ *               Function Declarations
+ ******************************************************/
+
+unsigned int toggle_bit(unsigned int value, unsigned char bit);
+
+/******************************************************
+ *            Interrupt Handlers Mapping
+ ******************************************************/
+
+/******************************************************
+ *               Variables Definitions
+ ******************************************************/
+
+/******************************************************
+ *               Function Definitions
+ ******************************************************/
+
+/**
+ * Main entry point 
+ */
+int main(int argc, char *argv)
 {
+    unsigned int data = 0xA5;
+    unsigned char bit = 6;
+    printf("Orginal vlaue is %X \n", data);
+    data = toggle_bit(data, bit);
+    printf("vlaue after bit %X toggle is  is %X \n", bit, data);
+    data = toggle_bit(data, bit);
+    printf("vlaue after bit %X toggle is  is %X \n", bit, data);
+
+    data = toggle_bit(data, bit);
+    printf("vlaue after bit %X toggle is  is %X \n", bit, data);
+    data = toggle_bit(data, bit);
+    printf("vlaue after bit %X toggle is  is %X \n", bit, data);
+    data = toggle_bit(data, bit);
+    printf("vlaue after bit %X toggle is  is %X \n", bit, data);
+
     return 0;
+}
+
+/**
+ * toggle a bit from a unigned int vlaue
+ *
+ * @param value[in]      : The unsigned int to be converted
+ * @param bit[in]        : The bit index
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: No protection for bits greater than 32  
+ *
+ * @return the bit toggled 
+ *
+ */
+unsigned int toggle_bit(unsigned int value, unsigned char bit)
+{
+    value ^= (1 << bit);
+    return value;
 }
