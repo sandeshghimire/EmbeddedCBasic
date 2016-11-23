6b86b95585d0686f02f86c9502e5ac7e597dfc9e
Sandesh Ghimire <sandeshghimire@outlook.com>
2016-11-23 11:34:51 -0800

    interrupts

--- src/interruptsHandling.c
+++ src/interruptsHandling.c
@@ -5,8 +5,7 @@
 
 /** @file
  * accessingFixedMemory.c
- * This Exmaple shows you how to access fixed memory form a given location 
-   Store a value of 0xA5A5 at a location 0x45676 
+ * This Exmaple shows how to handle interrupts 
  */
 
 /******************************************************
@@ -54,15 +53,15 @@
 /**
  * Main entry point 
  */
+
+#define __interrupt 
 int main(int argc, char *argv)
 {
 
-    int *ptr;             // create a pointer
-    ptr = (int *)0x45676; // refrence to a lcoation i.e. 0x45676
-    *ptr = 0xA5A5;        // store value 0xA5A5
-
-    // you can also do this in one step
-    *(int *const)0x45676 = 0xA5A5;
-
     return 0;
+}
+
+__interrupt void handle_isr(void) 
+{
+
 }
