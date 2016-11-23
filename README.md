b10201e48427723c1a686e275d3e445d5ba3c96a
Sandesh Ghimire <sandeshghimire@outlook.com>
2016-11-23 10:08:43 -0800

    Accessing Fixed Memory

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Basic embedded  C examples 
+
+Set Bit
+Clear Bit
+Toggle Bit
+Accessing Fixed Memory
+Constant Variables
+Data Declarations
+Infinite Loops
+Static Variables / Functions
+Typedef Example
+Interrupts Handling
+Preprocessor
--- src/accessingFixedMemory.c
+++ src/accessingFixedMemory.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+
+/*
+    This Exmaple shows you how to access fixed memory form a given location 
+    Store a value of 0xA5A5 at a location 0x45676 
+*/
+
 int main(int argc, char* argv)
 {
+
+   
+    int * ptr;              // create a pointer
+    ptr = ( int *) 0x45676; // refrence to a lcoation i.e. 0x45676
+    *ptr =  0xA5A5;         // store value 0xA5A5
+
+    // you can also do this in one step 
+    *(int * const )0x45676 = 0xA5A5;
+
     return 0;
 }
+
+
+
